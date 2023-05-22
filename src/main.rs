8c9ee1516059f8fec9b962b2032b80994ca727b0
Su_Pu#1A4472 <shown22327989@gmail.com>
2023-05-22 14:58:27 +0900

    create least carnel in Rust

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
 #![no_std]  //標準ライブラリのリンクを無効化
-fn main() {
-    
+#![no_main] //通常のエントリポイントを使用しない
+
+use core::panic::PanicInfo;
+
+//OSのエントリポイントを独自の_start関数で上書きしていく
+#[no_mangle]
+pub extern "C" fn _start() -> !{
+    loop {}
+}
+
+//この関数はパニック時に呼ばれる
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
